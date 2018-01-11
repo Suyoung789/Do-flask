44a17dc2a8d194517d3bd707bffdc71ea283d72d
Suyoung456 <rsy011203@gmail.com>
2018-01-11 16:01:43 +0900

    Modified

--- minitwit/minitwit.py
+++ minitwit/minitwit.py
@@ -1,11 +1,7 @@
 from flask import Flask
 
 #configuration
-DATABASE = './minitwit.db'
+DATABASE = '/minitwit.db'
 PER_PAGE = 30
 DEBUG = True
-SECRET_KEY = 'development key'
-
-app = Flask(__name__)
-app.config.from_object(__name__)
-app.config.from_envvar('MINITWIT_SETTINGS', silent=True)    
+SECERET_KEY = 'development key'

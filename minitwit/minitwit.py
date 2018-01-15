e1fabaceeb50e3ff1649f755691cb9bbd6ef7d8a
Suyoung456 <rsy011203@gmail.com>
2018-01-16 00:46:41 +0900

    미니트윗 뷰함수 구현 DB부분 함수 완료

--- minitwit/minitwit.py
+++ minitwit/minitwit.py
@@ -27,3 +27,14 @@ def teardown_request(exception):
     if hasattr(g, 'db'):
         g.db.close()
 
+def query_db(query, args=(), one=False):
+    cur = g.db.execute(query, args)
+    rv = [dict((cur.description[idx][0],value)for idx, value in enumerate(row))for row in cur.fetchall()]
+
+    return  (rv[0] if rv else None) if one else rv
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
